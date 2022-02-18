3acb47f6b926f28f6a749480bd68c25ce0d06590
rroye <rroye@naver.com>
2022-02-19 03:10:17 +0900

    2022-02-19
    도감 완성

---
+++ Assets/Game/Script/IllustratedController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IllustratedController : MonoBehaviour
+{
+    public SkillCardSo skillCardSo;
+    public SkillCardUI illDetailPanel;
+    public Sprite[] skillStartSpr;
+
+    void Start()
+    {
+        
+    }
+
+    public void IllCardBtn(int index)
+    {
+        illDetailPanel.SettingCard(skillStartSpr[index], skillCardSo.skillCards[index]);
+        illDetailPanel.gameObject.SetActive(true);
+    }
+}
